17e90f45bb3ecbce2cfb7cf18b1ed039962316de
liulonghai <liulonghai@qq.com>
2015-04-08 23:49:24 +0800

    kanstar: 使用raise函数，除零异常，name异常

--- Exception.py
+++ Exception.py
@@ -10,5 +10,38 @@ try:
 except StopIteration:
     print "Here is the end",i
 
-
 print 'HaHaHaHa'
+
+
+try:
+    print(a*2)
+except TypeError:
+    print("TypeError")
+except:
+    print("Not Type Error & Error noted")
+
+
+def test_func():
+    try:
+        m=1/0
+    except NameError:
+        print("Catch NameError in the sub-function")
+
+try:
+    test_func()
+except ZeroDivisionError:
+    print( "Catch error in the main program")
+
+print("LaLaLa")
+try:
+    raise StopIteration
+except StopIteration:
+    print "raise StopIteration"
+print("LaLaLa")
+
+print("LaLaLa")
+try:
+    raise StopIteration()
+except StopIteration:
+    print "raise StopIteration"
+print("LaLaLa")
